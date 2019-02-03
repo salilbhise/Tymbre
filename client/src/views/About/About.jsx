1cb05bee37be628597661ac64a9483f0e42ccfb7
Your Name <salil_bhise@yahoo.com>
2019-02-03 17:16:51 -0600

    borders and styling fun

--- client/src/views/About/About.jsx
+++ client/src/views/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row, Col, Container } from "react-bootstrap";
 import { Card } from "../../components/Card/Card.jsx";
 import { UserCard } from "../../components/UserCard/UserCard.jsx";
 import Button from "../../components/CustomButton/CustomButton.jsx";
@@ -7,70 +8,77 @@ import avatar from "../../assets/img/faces/face-3.jpg";
 
 
 function About() {
-  return (<div>
-    <UserCard
-      bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
-      avatar={avatar}
-      name="Mike Andrew"
-      userName="michael24"
-      description={
-        <span>
-          "Lamborghini Mercy
+  return (<div className="content">
+    <Container fluid>
+    <Row>
+      <Col md={6}>
+        <UserCard
+          bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
+          avatar={avatar}
+          name="Anthony Le"
+          userName="michael24"
+          description={
+            <span>
+              "Lamborghini Mercy
                     <br />
-          Your chick she so thirsty
+              Your chick she so thirsty
                     <br />
-          I'm in that two seat Lambo"
+              I'm in that two seat Lambo"
                   </span>
-      }
-      socials={
-        <div>
-          <Button simple>
-            <i className="fa fa-facebook-square" />
-          </Button>
-          <Button simple>
-            <i className="fa fa-twitter" />
-          </Button>
-          <Button simple>
-            <i className="fa fa-google-plus-square" />
-          </Button>
-        </div>
-      }
-    />
-
-    <UserCard
-      bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
-      avatar={avatar}
-      name="Mike Andrew"
-      userName="michael24"
-      description={
-        <span>
-          "Lamborghini Mercy
+          }
+          socials={
+            <div>
+              <Button simple>
+                <i className="fa fa-facebook-square" />
+              </Button>
+              <Button simple>
+                <i className="fa fa-twitter" />
+              </Button>
+              <Button simple>
+                <i className="fa fa-google-plus-square" />
+              </Button>
+            </div>
+          }
+        />
+      </Col>
+      <Col md={6}>
+        <UserCard
+          bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
+          avatar={avatar}
+          name="Salil Bhise"
+          userName="michael24"
+          description={
+            <span>
+              "Lamborghini Mercy
                     <br />
-          Your chick she so thirsty
+              Your chick she so thirsty
                     <br />
-          I'm in that two seat Lambo"
+              I'm in that two seat Lambo"
                   </span>
-      }
-      socials={
-        <div>
-          <Button simple>
-            <i className="fa fa-facebook-square" />
-          </Button>
-          <Button simple>
-            <i className="fa fa-twitter" />
-          </Button>
-          <Button simple>
-            <i className="fa fa-google-plus-square" />
-          </Button>
-        </div>
-      }
-    />
+          }
+          socials={
+            <div>
+              <Button simple>
+                <i className="fa fa-facebook-square" />
+              </Button>
+              <Button simple>
+                <i className="fa fa-twitter" />
+              </Button>
+              <Button simple>
+                <i className="fa fa-google-plus-square" />
+              </Button>
 
+            </div>
+          }
+        />
+      </Col>
+      </Row>
+      </Container>
   </div>)
-}
-
-
-
-
-
+  }
+  
+  
+  
+  
+  
 export default About;
